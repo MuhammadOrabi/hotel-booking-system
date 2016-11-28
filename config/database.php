4686446602eb62af379b94bb0b89a730ddc2f2b7
Muhammad Orabi <muhammad.orabi.2014@hotmail.com>
2016-11-28 02:04:33 +0200

    database

--- config/database.php
+++ config/database.php
@@ -2,14 +2,7 @@
 
 return [
     
-    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-    $host = $url["host"];
-    $username = $url["user"];
-    $password = $url["pass"];
-    $database = substr($url["path"], 1);
-
-
+    
     /*
     |--------------------------------------------------------------------------
     | PDO Fetch Style
